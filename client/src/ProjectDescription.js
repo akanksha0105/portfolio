367c382a8316072ad28bc37335da4e865c041868
Akanksha <mittalakanksha0105@gmail.com>
2024-10-18 20:26:28 +0530

    updated resume url

--- client/src/ProjectDescription.js
+++ client/src/ProjectDescription.js
@@ -5,43 +5,43 @@ import "./ProjectDescription.css";
 
 function ProjectDescription() {
 	return (
-		<div className="project__description">
-			<div className="project__name">
+		<div className='project__description'>
+			<div className='project__name'>
 				kittyshare
-				<a href=""></a>
+				<a href=''></a>
 			</div>
 
-			<div className="box">
-				<div className="project__card__description">
+			<div className='box'>
+				<div className='project__card__description'>
 					A web application that allows a user to transmit URL seamlessly across
 					the devices. It offers offline messaging with the help of push
 					notifications
 				</div>
 			</div>
 
-			<div className="description">
+			<div className='description'>
 				A web application that allows a user to transmit URL seamlessly across
 				the devices. It offers offline messaging with the help of push
 				notifications
 			</div>
-			<div className="project__technologies">
+			<div className='project__technologies'>
 				<div>React</div>
 				<div>Express</div>
 				<div>Node.js</div>
 				<div>Push API</div>
 			</div>
-			<div className="project__link__icons">
+			<div className='project__link__icons'>
 				<a
-					className="gitlab__link"
-					href="https://gitlab.com/mittalakanksha0105/kitty-share">
+					className='gitlab__link'
+					href='https://github.com/akanksha0105/kitty-share'>
 					<i
-						class="fa fa-gitlab"
+						class='fa fa-gitlab'
 						style={({ fontSize: "48px" }, { color: "black" })}></i>
 				</a>
 
-				<a className="external" href="https://kittyshare.xyz/">
+				<a className='external' href='https://kitty-share.vercel.app/'>
 					<i
-						class="fa fa-external-link"
+						class='fa fa-external-link'
 						style={({ fontSize: "48px" }, { color: "black" })}></i>
 				</a>
 			</div>
